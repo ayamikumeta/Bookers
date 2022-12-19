45fa28b2ab4d71ae938c0c8a868072fa489ab7fd
ayamikumeta <117357774+ayamikumeta@noreply.github.com>
2022-12-19 08:37:26 +0000

    2nd Commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,19 +1,37 @@
 class BooksController < ApplicationController
-
-  def index
-    @books = Book.all
+  def new
+    @book = Book.new
   end
 
   def create
-    book = Book.new(book_params)
-    if book.save
-    redirect_to '/books/params(:id)'
+    @book = Book.new(book_params)
+    @book.save
+    redirect_to "/books/#{@book.id}"
   end
 
-  def edit
+  def index
+    @book = Book.new
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    book.uptade(params.id)
+    redirect_to "/books"
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    book.destroy
+    redirect_to "/books"
   end
 
   private
@@ -21,5 +39,5 @@ class BooksController < ApplicationController
   def book_params
     params.require(:book).permit(:title, :body)
   end
- end
 end
+
