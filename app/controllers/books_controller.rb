329056730487bc753f46fb1542e698e5836d1c1d
ayamikumeta <117357774+ayamikumeta@noreply.github.com>
2022-12-22 08:13:02 +0000

    5th Commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,7 +7,8 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @books = Book.all
     if @book.save
-     redirect_to book_path(@book.id)
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
     else
      render :index
   end
@@ -28,9 +29,13 @@ class BooksController < ApplicationController
   end
 
   def update
-    @books = Book.new(book_params)
     @book = Book.find(params[:id])
-    @book.update(book_params)
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+     render :edit
+   end
   end
 
   def destroy
